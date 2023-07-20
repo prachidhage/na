e28dceef9698e1bf2e7c2dc7dacd162e1a2070b0
prachidhage <prachidhage3@gmail.com>
2023-07-20 09:45:00 +0000

    ..

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var request = require('supertest');
 var app = require('.../index.js');describe('GET /', function () {
 it('respond with hello world', function(done) {
-request(app).get('/').expect('hello loker', done);
+request(app).get('/').expect('hello er', done);
 });
 });
