486f83350a1845d2395d9b3e731257d16f28eb78
prachidhage <prachidhage3@gmail.com>
2023-07-20 09:29:49 +0000

    3 node

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express= require('express');
 var app = express();
 app.get('/', function (req, res) {
-res.send('hello wor..ld');
+res.send('hello worlds people');
 });
 app.listen(process.env.PORT || 80);
 module.exports = app;
